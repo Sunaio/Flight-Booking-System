72238b9682631192df0bf3f8c43ed959f74093eb
Sunaio <finalsecond21@gmail.com>
2026-03-31 21:48:01 -0400

    changed and add first 10 flight from database

--- flightapi.py
+++ flightapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import os
 import pyodbc
 
@@ -8,18 +8,32 @@ def get_connection():
     connection_str = os.getenv("DB_ACCESS_KEY")
     return pyodbc.connect(connection_str)
 
-@app.get("/")
-def home():
-    return {"status": "API running"}
+@app.get("/flights")
+def get_flights(
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100)
+):
+    conn = get_connection()
+    cursor = conn.cursor()
 
-@app.get("/data")
-def get_data():
-    connect = get_connection()
-    cursor = connect.cursor()
-    cursor.execute("SELECT TOP 10 * FROM dbo.table.flight_data")
+    offset = (page - 1) * page_size
+
+    query = """
+        SELECT *
+        FROM dbo.flight_data
+        ORDER BY (time)
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY
+    """
+
+    cursor.execute(query, offset, page_size)
 
     columns = [col[0] for col in cursor.description]
     rows = cursor.fetchall()
 
-    connect.close()
-    return [dict(zip(columns, row)) for row in rows]
+    conn.close()
+
+    return {
+        "page": page,
+        "page_size": page_size,
+        "data": [dict(zip(columns, row)) for row in rows]
+    }
