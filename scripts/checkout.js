4f57db7403b36aca9c6198cb5b0e53aa1c5a2891
Sunaio <finalsecond21@gmail.com>
2026-04-23 19:25:47 -0400

    added round-trip functionality to checkout and frontend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,6 +7,14 @@ try {
 }
 
 let seatMap = {};
+let returnMap = {};
+let returnId = null;
+let start = 1;
+let end = 5;
+const pageSize = 5;
+
+const resultsDiv = document.getElementById("flightResults");
+const pageInfo = document.getElementById("pageInfo");
 
 // 12-hour Time formatting
 function formatTime(timeStr) {
@@ -30,26 +38,51 @@ document.getElementById("time").textContent = flight.departure_time && flight.ar
 const ROWS = 21;
 const COLS = ["A", "B", "C", "D", "E", "F"];
 
-async function loadSeats() {
+async function loadSeats(isRound) {
     try {
-        const response = await fetch(`https://flightapi-hbcdfpabdqhqbudb.eastus-01.azurewebsites.net/flights/${flight.id}/seats`);
+        let url = `https://flightapi-hbcdfpabdqhqbudb.eastus-01.azurewebsites.net/flights/`;
+        if(isRound) {
+            url += `${returnId}/seats`;
+        }else {
+            url += `${flight.id}/seats`;
+        }
+        const response = await fetch(url);
         const data = await response.json();
-        seatMap = {};
-        data.seats.forEach(seat => {
-            seatMap[seat.seat_number] = !seat.is_available;
+        let seats = data.seats;
+        if (!seats) {
+            console.error("No seats found in response:", data);
+            return;
+        }
+
+        let map = {};
+        seats.forEach(seat => {
+            map[seat.seat_number] = !seat.is_available;
         });
+
+        if(isRound) {
+            returnMap = map;
+        }else {
+            seatMap = map;
+        }
     } catch (err) {
         console.error("Failed to load seat information:", err);
     }
 }
 
 let selectedSeat = null;
+let returnSeat = null;
 const baseCost = flight.cost;
 let seatFee = 0;
+let returnFee = 0;
 let costMult = 1;
 
-function buildSeatGrid(cabin) {
-    const grid = document.getElementById("seatGrid");
+function buildSeatGrid(cabin, isRound) {
+    let grid;
+    if(isRound) {
+        grid = document.getElementById("returnSeatGrid");
+    }else {
+        grid = document.getElementById("seatGrid");
+    }
     grid.innerHTML = "";
 
     const startRow = cabin === "first" ? 1 : cabin === "business" ? 4 : 8;
@@ -74,7 +107,12 @@ function buildSeatGrid(cabin) {
             }
 
             const seatId = `${r}${col}`;
-            const taken  = seatMap[seatId] === true;
+            let taken;
+            if(isRound) {
+                taken = returnMap[seatId] === true;
+            }else {
+                taken = seatMap[seatId] === true;
+            }
 
             const seat = document.createElement("div");
             seat.className = "seat" + (taken ? " taken" : "");
@@ -82,8 +120,14 @@ function buildSeatGrid(cabin) {
             seat.textContent = seatId;
 
             if (!taken) {
-                seat.addEventListener("click", () => selectSeat(seat, seatId));
-                if (selectedSeat === seatId) seat.classList.add("selected");
+                seat.addEventListener("click", () => selectSeat(seat, seatId, isRound));
+                if(isRound && returnSeat === seatId) {
+                    seat.classList.add("selected");
+                }
+
+                if (!isRound && selectedSeat === seatId) {
+                    seat.classList.add("selected");
+                }
             }
             rowEl.appendChild(seat);
         });
@@ -93,28 +137,57 @@ function buildSeatGrid(cabin) {
 }
 
 function updateCost() {
-    const totalCost = (baseCost * costMult) + seatFee;
+    const totalCost = (baseCost * costMult) + seatFee + returnFee;
     document.getElementById("flightCost").textContent = "$" + totalCost;
 }
 
-function selectSeat(el, seatId) {
-    document.querySelectorAll(".seat.selected").forEach(s => s.classList.remove("selected"));
+function clearSelectedSeats(gridId) {
+    document.querySelectorAll(`#${gridId} .seat.selected`)
+        .forEach(s => s.classList.remove("selected"));
+}
+
+function selectSeat(el, seatId, isRound) {
+    let gridId = isRound ? "returnSeatGrid" : "seatGrid";
+    clearSelectedSeats(gridId);
     el.classList.add("selected");
-    selectedSeat = seatId;
-    document.getElementById("selectedSeatDisplay").textContent = seatId;
-    let cabin = document.querySelector(".tab.active").dataset.class;
-    seatFee = 0;
+    if(isRound) {
+        returnSeat = seatId;
+        document.getElementById("returnSelectedSeatDisplay").textContent = seatId;
+    }else {
+        selectedSeat = seatId;
+        document.getElementById("selectedSeatDisplay").textContent = seatId;
+    }
+    let cabin;
+    if(isRound) {
+        cabin = document.querySelector(".rtab.active").dataset.class
+    }else {
+        cabin = document.querySelector(".tab.active").dataset.class;
+    }
+    let fee = 0;
     // Economy base - Free, Window +$25
     // Business +$400 fee, All seats free
     // First class +$600 fee, All seats free
-    if (cabin === "economy") {
+    // Return Seat back will be free of charge
+    // Still fees for > economy class for return flight
+    if (cabin === "economy" && !isRound) {
         if (seatId.endsWith("A") || seatId.endsWith("F")) {
-            seatFee = 25;
+            fee = 25;
         }
     }
 
-    if (cabin === "business") seatFee = 400;
-    if (cabin === "first") seatFee = 600;
+    if (cabin === "business") fee = 400;
+    if (cabin === "first") fee = 600;
+
+    if(isRound) {
+        returnSeat = seatId;
+        document.getElementById("returnSelectedSeatDisplay").textContent = seatId;
+        returnFee = fee;
+    }else {
+        selectedSeat = seatId;
+        document.getElementById("selectedSeatDisplay").textContent = seatId;
+        seatFee = fee;
+    }
+    
     updateCost();
 }
 
@@ -123,7 +196,7 @@ document.querySelectorAll(".tab").forEach(tab => {
     tab.addEventListener("click", () => {
         document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
         tab.classList.add("active");
-        buildSeatGrid(tab.dataset.class);
+        buildSeatGrid(tab.dataset.class, false);
         // Reset seat selection and cost when switching cabins
         selectedSeat = null;
         seatFee = 0;
@@ -132,15 +205,33 @@ document.querySelectorAll(".tab").forEach(tab => {
     });
 });
 
-async function renderSeats(cabin) {
-    const grid = document.getElementById("seatGrid");
+document.querySelectorAll(".rtab").forEach(tab => {
+    tab.addEventListener("click", () => {
+        document.querySelectorAll(".rtab").forEach(t => t.classList.remove("active"));
+        tab.classList.add("active");
+        buildSeatGrid(tab.dataset.class, true);
+        // Reset seat selection and cost when switching cabins
+        returnSeat = null;
+        returnFee = 0;
+        document.getElementById("returnSelectedSeatDisplay").textContent = "None";
+        updateCost();
+    });
+});
+
+async function renderSeats(cabin, isRound) {
+    let grid;
+    if(isRound) {
+        grid = document.getElementById("returnSeatGrid")
+    }else {
+        grid = document.getElementById("seatGrid");
+    }
     grid.innerHTML = "<p><br>Fetching Seats...</p>";
-    await loadSeats();
-    buildSeatGrid(cabin);
+    await loadSeats(isRound);
+    buildSeatGrid(cabin, isRound);
 }
 
 async function init() {
-    await renderSeats("economy");
+    await renderSeats("economy", false);
 }
 
 init();
@@ -153,6 +244,11 @@ document.getElementById("confirmButton").addEventListener("click", (e) => {
         return;
     }
 
+    if(!returnSeat && !document.querySelector(".seat-section").hidden) {
+        alert("Please select a return seat before confirming.");
+        return;
+    }
+
     const firstName = document.getElementById("firstName").value.trim();
     const lastName  = document.getElementById("lastName").value.trim();
     if (!firstName || !lastName) {
@@ -161,14 +257,44 @@ document.getElementById("confirmButton").addEventListener("click", (e) => {
     }
 
     // Build confirmation message
-    const route = `${flight.dep_airport || "?"} → ${flight.arr_airport || "?"}`;
-    document.getElementById("confirmSummary").innerHTML =
-        `Name: ${firstName} ${lastName} <br>
-        ${route} <br>
-        Seat: ${selectedSeat} <br>
-        Cost: ${document.getElementById("flightCost").textContent} <br>
-        Type: ${document.getElementById("type").value === "round" ? "Round-Trip" : "One-Way"}`;
+    const type = document.getElementById("type").value === "round" ? "Round-Trip" : "One-Way";
+    const cost = document.getElementById("flightCost").textContent;
+    const departingRoute = `${flight.dep_airport || "?"} → ${flight.arr_airport || "?"}`;
+    let returnRoute = "";
+    if (returnSeat) {
+        returnRoute = `${flight.arr_airport || "?"} → ${flight.dep_airport || "?"}`;
+    }
 
+    document.getElementById("confirmSummary").innerHTML = `
+        <div class="confirm-wrapper">
+            <div class="confirm-card">
+                <h3>Departing Flight</h3>
+                <div class="confirm-row"><span>Route</span><span>${departingRoute}</span></div>
+                <div class="confirm-row"><span>Seat</span><span>${selectedSeat}</span></div>
+            </div>
+            ${type === "Round-Trip" ? `
+            <div class="confirm-card">
+                <h3>Return Flight</h3>
+                <div class="confirm-row"><span>Route</span><span>${returnRoute}</span></div>
+                <div class="confirm-row"><span>Seat</span><span>${returnSeat}</span></div>
+            </div>
+            ` : ""}
+            <div class="confirm-card summary">
+                <h3>Summary</h3>
+                <div class="confirm-row">
+                    <span>Name</span><span>${firstName} ${lastName}</span></div>
+                </div>
+                <div class="confirm-row">
+                    <span>Trip Type</span>
+                    <span>${type}</span>
+                </div>
+                <div class="confirm-row total">
+                    <span>Total Cost</span>
+                    <span>${cost}</span>
+                </div>
+            </div>
+        </div>
+`;
     document.getElementById("confirmPopup").hidden = false;
 });
 
@@ -180,6 +306,8 @@ document.querySelector("#confirmPopup .submit-btn").addEventListener("click", as
             body: JSON.stringify({
                 flight_id: flight.id,
                 seat_number: selectedSeat,
+                return_id: returnId || null,
+                return_seat: returnSeat || null
             })
         });
         if (!response.ok) {
@@ -194,7 +322,134 @@ document.querySelector("#confirmPopup .submit-btn").addEventListener("click", as
     }
 });
 
-document.getElementById("type").addEventListener("change", (e) => {
-    costMult = (e.target.value === "round") ? 2 : 1;
+document.getElementById("type").addEventListener("change", async (e) => {
+    const isRoundTrip = e.target.value === "round";
+    costMult = isRoundTrip ? 2 : 1;
     updateCost();
+
+    const resultsSect = document.querySelector(".results-section");
+    const seatsSect = document.querySelector(".seat-section");
+    if(isRoundTrip) {
+        await loadReturnFlight();
+        resultsSect.hidden = false;
+    }else {
+        resultsSect.hidden = true;
+        seatsSect.hidden = true;
+    }
 });
+
+async function loadReturnFlight() {
+    try {
+        const url = `https://flightapi-hbcdfpabdqhqbudb.eastus-01.azurewebsites.net/flight/filters?dep_airport=${flight.arr_airport}&arr_airport=${flight.dep_airport}`;
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`HTTP error: ${res.status}`);
+        }
+        const data = await res.json();
+        renderFlights(data.data);
+        pageInfo.textContent = `Page ${data.pagination.page}`;
+    } catch (err) {
+        console.error("Failed to load flights:", err);
+        resultsDiv.innerHTML = "<p style='color:red'>Failed to load flights</p>";
+    }
+}
+
+function getAirlineLogo(airline) {
+    const logos = {
+        "United Airlines": "assets/unitedlogo.png",
+        "Delta Airlines": "assets/Delta-Logo.png",
+        "American Airlines": "assets/americanlogo.jpg",
+        "Spirit Airlines": "assets/Spirit-Airlines-logo.png",
+        "Southwest Airlines": "assets/southwestlogo.png",
+    };
+
+    return logos[airline] || "assets/nothing.png";
+}
+
+async function getSeats(flightId) {
+    try {
+        const res = await fetch(`https://flightapi-hbcdfpabdqhqbudb.eastus-01.azurewebsites.net/flights/${flightId}/seats/summary`);
+        if (!res.ok) {
+            throw new Error(`HTTP error: ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.error("Failed to load seat information:", err);
+        return {
+            flight_id: flightId,
+            total_seats: 0,
+            unbooked_seats: 0,
+            booked_seats: 0
+        };
+    }
+}
+
+function renderFlights(flights) {
+    resultsDiv.innerHTML = "";
+    if (!flights || flights.length === 0) {
+        resultsDiv.innerHTML = "<p>No flights found.</p>";
+        return;
+    }
+    flights.forEach(flight => {
+        const card = document.createElement("div");
+        card.className = "flight-card";
+        card.innerHTML = `
+            <div class="flight-left">
+                <div class="airline-sec">
+                    <img class="airline-logo" src="${getAirlineLogo(flight.owner)}" alt="${flight.owner || "Airline Logo"}">
+                    <span class="airline-name">${flight.owner || "Unknown Airline"}</span>
+                </div>
+                <div class="flight-num">
+                    <span>${flight.flight_number || "Unknown Flight Number"}</span>
+                </div>
+                <div class="plane-info">
+                    <span>${flight.plane_type || "Unknown Plane Type"}</span>
+                    <span>${flight.plane_icao || "Unknown ICAO"}</span>
+                </div>
+                <div class="route_iata">
+                    <span>${flight.dep_airport || "Unknown Departure Airport"}</span>
+                    <span class="route-arrow">→</span>
+                    <span>${flight.arr_airport || "Unknown Arrival Airport"}</span>
+                </div>
+                <div class="route_name">
+                    <span>${flight.dep_airport_name || "Unknown Departure Airport Name"}</span>
+                    <span class="route-arrow">→</span>
+                    <span>${flight.arr_airport_name || "Unknown Arrival Airport Name"}</span>
+                </div>
+                <div class="seats">
+                    <span> Loading seats... </span>
+                </div>
+                <div class="date">
+                    <span>${flight.departure_date || "Unknown Date"}</span>
+                </div>
+                <div class="times">
+                    <span>${formatTime(flight.departure_time)} → ${formatTime(flight.arrival_time)}</span>
+                </div>
+            </div>
+            <div class="flight-right">
+                <button class="book-btn">Select</button>
+            </div>
+        `;
+        
+        const bookBtn = card.querySelector(".book-btn");
+        bookBtn.addEventListener("click", async () => {
+            returnId = flight.flight_id;
+            const resultsSect = document.querySelector(".results-section");
+            const seatsSect = document.querySelector(".seat-section");
+            await renderSeats("economy", true);
+            resultsSect.hidden = true;
+            seatsSect.hidden = false;
+        });
+
+        const seatsDiv = card.querySelector(".seats");
+        getSeats(flight.flight_id).then(seatInfo => {
+            const seatText = `👤 ${seatInfo.booked_seats}/${seatInfo.total_seats}`;
+            seatsDiv.textContent = seatText;
+        }).catch(() => {
+            seatsDiv.textContent = "Seats: Unknown";
+        });
+
+        resultsDiv.appendChild(card);
+    });
+}
